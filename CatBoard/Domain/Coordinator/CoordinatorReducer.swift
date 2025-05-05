55be4f7b400a0fdcf5e7bee4a5b5814eed44284e
terrio32 <atora.has75@gmail.com>
2025-05-05 14:50:47 +0900

    Galleryドメインをリファクタリングし画像リポジトリロジックを分離

--- CatBoard/Domain/Coordinator/CoordinatorReducer.swift
+++ CatBoard/Domain/Coordinator/CoordinatorReducer.swift
@@ -1,19 +1,26 @@
 import ComposableArchitecture
+import SwiftUI
 
 @Reducer
 struct CoordinatorReducer {
-    typealias State = CoordinatorState
+
+    @ObservableState
+    struct State: Equatable {
+        // 他のルートレベルの状態（例：パスナビゲーションなど）
+        var gallery: GalleryReducer.State = .init()
+    }
+
     typealias Action = CoordinatorAction
 
-    // MARK: - Reducer Body
-
     var body: some ReducerOf<Self> {
+        // Galleryドメインへのスコープ
         Scope(state: \.gallery, action: \.gallery) {
             GalleryReducer()
         }
 
         Reduce { _, action in
             switch action {
+                // Coordinator固有のアクション処理
                 case .gallery:
                     return .none
             }
