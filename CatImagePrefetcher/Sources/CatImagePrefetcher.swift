9bf38d71802d75fffa2d3c063671c8dc5b58c454
akitorahayashi <atora.has75@gmail.com>
2025-07-31 17:32:24 +0900

    Prefetcherの軽微な修正

--- CatImagePrefetcher/Sources/CatImagePrefetcher.swift
+++ CatImagePrefetcher/Sources/CatImagePrefetcher.swift
@@ -112,9 +112,9 @@ public actor CatImagePrefetcher: CatImagePrefetcherProtocol {
             attempts += 1
 
             // 6. ログ出力
-            try await print(
+            print(
                 "プリフェッチ進捗: \(loadedImages.count)枚中\(screenedModels.count)枚通過 "
-                    + "(現在\(getPrefetchedCount())枚)"
+                    + "(現在\(try await getPrefetchedCount())枚)"
             )
         }
     }
